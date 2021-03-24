e2e363f59d19f2a4396f76cec0e96b8b46fe7c87
emi ochoa <eoqwerty@gmail.com>
2021-03-24 18:52:56 -0400

    Wrote xml for login screen, changed main activity to represent this

--- app/src/main/java/com/example/bismapp/MainActivity.java
+++ app/src/main/java/com/example/bismapp/MainActivity.java
@@ -19,8 +19,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        /*
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+
 
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -30,6 +33,9 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        */
+
     }
 
     @Override
