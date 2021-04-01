68d504e01529cd7835c51bb225fe9dff5ec4fba9
Keidai Lee <sigrick15@MacBook-Pro-de-Keidai.local>
2021-04-01 14:06:35 -0400

    fixed create teams formats. Adding drop down menu but still doesn't work and app quits sporadically

--- app/src/main/java/com/example/bismapp/ui/modifyTeams/TeamInfoFragment.java
+++ app/src/main/java/com/example/bismapp/ui/modifyTeams/TeamInfoFragment.java
@@ -7,9 +7,12 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 
 import com.example.bismapp.MainActivity;
 import com.example.bismapp.R;
+import com.example.bismapp.TeamMember;
 import com.example.bismapp.TeamMemberAdapter;
 
 public class TeamInfoFragment extends Fragment {
@@ -19,6 +22,23 @@ public class TeamInfoFragment extends Fragment {
                              Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.fragment_team_info, container, false);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getContext(),
+                R.layout.activity_list_view, teamMemberNames());
+
+        AutoCompleteTextView textView = (AutoCompleteTextView)
+                view.findViewById(R.id.enterAssociateName);
+
+        textView.setAdapter(adapter);
+
         return view;
+    }
+
+    private String[] teamMemberNames() {
+        String[] names = new String[MainActivity.teamMembers.size()];
+        for(int i = 0; i < names.length; i++) {
+          names[i] = MainActivity.teamMembers.get(i).getName();
+        }
+        return names;
     }
 }
