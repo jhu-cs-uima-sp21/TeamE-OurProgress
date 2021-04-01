fbec2478c5275f28813829b15e49cad612b428dc
Keidai Lee <sigrick15@MacBook-Pro-de-Keidai.local>
2021-04-01 15:52:29 -0400

    added new starting Database filesgit add . Whoopee

---
+++ app/src/main/java/com/example/bismapp/model/DataItem.java
@@ -0,0 +1,31 @@
+package com.example.bismapp.model;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class DataItem implements Parcelable {
+
+    protected DataItem(Parcel in) {
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    public static final Creator<DataItem> CREATOR = new Creator<DataItem>() {
+        @Override
+        public DataItem createFromParcel(Parcel in) {
+            return new DataItem(in);
+        }
+
+        @Override
+        public DataItem[] newArray(int size) {
+            return new DataItem[size];
+        }
+    };
+}
